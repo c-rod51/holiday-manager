f1fdc60b94f4b518263c0146bdb1895c3be5f4cf
Christian <c_rod51@yahoo.com>
2022-07-09 12:52:46 -0700

    AddHoliday() functionality

---
+++ holiday-manager.py
@@ -0,0 +1,66 @@
+#Import relevant packages
+from datetime import datetime
+import json
+from bs4 import BeautifulSoup
+import requests
+from dataclasses import dataclass
+import inspect
+
+#Holiday class
+##
+@dataclass
+class Holiday:
+    name: str
+    date: datetime
+        
+    def __str__ (self):
+        # String output
+        date_format = '%Y-%m-%d'
+        date_str = datetime.strftime(self.date, date_format)
+        # Holiday output when printed.
+        return '%s (%s) ' % (self.name, date_str)
+
+#HolidayList Class
+
+class HolidayList:
+    def __init__(self):
+        self.innerHolidays = []
+   
+    def addHoliday(self, holidayObj):
+        # Make sure holidayObj is an Holiday Object by checking the type
+        if isinstance(holidayObj, Holiday):
+            # Use innerHolidays.append(holidayObj) to add holiday
+            self.innerHolidays.append(holidayObj)
+            # print to the user that you added a holiday
+            print(f'Success: {holidayObj} has been added to the holiday list.')
+        else:
+            print('Error: Please try again.')
+
+
+
+
+#UI Add Holiday
+##
+def AddHoliday(holiday_list):
+    #Print welcome message
+    print(f'''
+        Add a Holiday
+        ==============
+    ''')
+    #Gather Holiday name from user
+    holiday_name = str(input("Holiday: "))
+    #Gather Holiday date from user and ensure datetime
+    incorrect_form = True
+    while incorrect_form:
+        try:
+            testdate = input('Enter date (YYYY-MM-DD): ')
+            date_format = '%Y-%m-%d'
+            date = datetime.strptime(testdate, date_format).date()
+        except ValueError:
+            print('Invalid date. Please try again.')
+        else:
+            incorrect_form = False
+            holiday_object = Holiday(holiday_name, date)
+            holiday_list.addHoliday(holiday_object)
+            
+    #MainMenu()
